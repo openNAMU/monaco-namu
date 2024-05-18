c56624e13bc67e1fff992df677e2054783efbc57
잉여개발기 <min08101@naver.com>
2024-05-18 16:17:13 +0900

    오픈나무에 쓸 수 있도록 커스텀

--- namu/vs/languages/namumark.js
+++ namu/vs/languages/namumark.js
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-export default function(monaco) {
+function opennamu_monaco_custom() {
     monaco.languages.register({
         id: 'namumark',
         aliases: [
